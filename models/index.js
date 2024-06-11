0eb9e0571f4c7e3c9f7d343846074548461482e0
XII IPS D 20 Muhammad Nabil Sholahudin <masda.mlers@gmail.com>
2024-06-11 17:09:46 +0700

    Add Vercel Versi 9999

--- models/index.js
+++ models/index.js
@@ -10,12 +10,6 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
-const mysql2 = require('mysql2')
-
-if (options.dialect === 'mysql') {
-  options.dialectModule = mysql2;
-}
-
 
 let sequelize;
 if (config.use_env_variable) {
