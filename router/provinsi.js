0eccdcad46914b5d02711cbe2b3e41013101d3ec
XII IPS D 20 Muhammad Nabil Sholahudin <masda.mlers@gmail.com>
2024-06-12 08:59:57 +0700

    Edit Logika Destinasi

--- router/provinsi.js
+++ router/provinsi.js
@@ -5,11 +5,11 @@ const { checkRole } = require("../middleware/authVerify");
 const routeProvinsi = express.Router();
 
 routeProvinsi.post('/add-province',(req,res,next) => checkRole(req,res,next, 'Admin'),addDataDestinationController.province)
-routeProvinsi.get('/province',(req,res,next) => checkRole(req,res,next, 'Admin'),destinationPlaces.province)
-routeProvinsi.get('/detail',(req,res,next) => checkRole(req,res,next, 'Admin'),destinationPlaces.detail)
-routeProvinsi.get('/photo',(req,res,next) => checkRole(req,res,next, 'Admin'),destinationPlaces.getPhoto)
-routeProvinsi.get('/recomendation',(req,res,next) => checkRole(req,res,next, 'Admin'),destinationPlaces.recomendation)
-routeProvinsi.get('/get-map',(req,res,next) => checkRole(req,res,next, 'Admin'),destinationPlaces.getMap)
+routeProvinsi.get('/province',(req,res,next) => checkRole(req,res,next, 'User'),destinationPlaces.province)
+routeProvinsi.get('/detail',(req,res,next) => checkRole(req,res,next, 'User'),destinationPlaces.detail)
+routeProvinsi.get('/photo',(req,res,next) => checkRole(req,res,next, 'User'),destinationPlaces.getPhoto)
+routeProvinsi.get('/recomendation',(req,res,next) => checkRole(req,res,next, 'User'),destinationPlaces.recomendation)
+routeProvinsi.get('/get-map',(req,res,next) => checkRole(req,res,next, 'User'),destinationPlaces.getMap)
 
 
 
